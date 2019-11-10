22c498a0f8f849dfc862c975a3e1010f7edde2fe
hextion <100ishundred@gmail.com>
2019-11-10 22:41:16 +0300

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   env: {
     commonjs: true,
-    es6: true
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {}
+  root: true,
 };
